084734e3bd5137e04c35ec1d3cc203a8f21d45f3
My Name <myEmail@example.com>
2022-10-04 18:58:00 +0300

    Задачи в C#

---
+++ Tasks/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+//  Задача1. Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2) 
+{
+    Console.WriteLine("max = "  + n1 );
+}
+else 
+{
+    Console.WriteLine("max = "  + n2 );
+}
+*/
+/*
+//Задача 2: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (n1 > max) max = n1;
+if (n2 > max) max = n2;
+if (n3 > max) max = n3;
+
+Console.Write("max=");
+Console.WriteLine( max );
+*/
+
+/*
+//Задача 3: Напишите программу, которая на вход принимает число
+//         и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+ Console.Write("Input namber: ");
+ double namber = Convert.ToUInt32(Console.ReadLine());
+
+ if (namber % 2 == 0)
+ {
+    Console.WriteLine("Yes");
+ }
+ else 
+ {
+    Console.WriteLine("No");
+ }
+*/
+
+/*
+// Задача 8: Напишите программу, которая на вход принимает число (N),
+//           а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input a namber ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current+=2;
+}
+*/
